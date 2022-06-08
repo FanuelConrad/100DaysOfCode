0a2054a2bbc176984ec3f22d4574d383fdc68e2c
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2022-06-08 09:15:30 +0300

    Calculated Min, Max, Sum, Count and Average using System.Linq

---
+++ Day 001/Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args){
+            int[] numbers = {51,1,3,4,98};
+
+            //get the minimum element
+            Console.WriteLine("Smallest Element: "+numbers.Min());
+
+            //Max() returns the largest number in array
+            Console.WriteLine("Largest Element: "+ numbers.Max());
+
+            //get the sum of all array elements
+            Console.WriteLine("Sum: " + numbers.Sum());
+
+            //get the total number of elements present in the array
+            Console.WriteLine("Count: " + numbers.Count());
+
+            //get the average of the elements
+            Console.WriteLine("Average: " + numbers.Average());
+
+            Console.ReadLine();
+        }
+    }
+}
