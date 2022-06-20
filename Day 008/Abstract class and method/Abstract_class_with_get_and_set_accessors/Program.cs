0804d33c6a671fc3cf2ddc05b1ba4a84d468f9fe
Fanuel Conrad <59276995+FanuelConrad@users.noreply.github.com>
2022-06-20 08:16:57 +0300

    Added abstraction

---
+++ Day 008/Abstract class and method/Abstract_class_with_get_and_set_accessors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AbstractClass // Note: actual namespace depends on the project name.
+{
+    abstract class Animal
+    {
+        protected string name;
+
+        //abstract method
+        public abstract string Name{get; set;}
+
+    }
+
+    //inheriting from abstract class
+    class Dog : Animal
+    {
+        //provide implementation of abstract method
+        public override string Name {get {return name;} set {name = value;} }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object of Dog class
+            Dog obj = new Dog();
+            obj.Name = "Tom";
+            Console.WriteLine("Name: " + obj.Name);;
+
+            Console.ReadLine();
+        }
+    }
+}
